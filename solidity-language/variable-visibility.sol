f01a5f92a8a6cb94d1d7057c3de888a29c41ded9
gabiroba <gabrielcardosogirarde@gmail.com>
2024-09-27 10:35:46 -0300

    variable visibilities

--- solidity-language/variable-visibility.sol
+++ solidity-language/variable-visibility.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.27;
 
 contract variableVisibility {
-    
+
+    // internal - Só visível dentro do contrato e derivados (Herança)
+    string internal nameInternal = "nameInternal";
+
+    // private - Vísivel somente dentro do contrato
+    string private namePrivate = "namePrivate";
+
+    // public - Acessível de qualquer jeito
+    string public namePublic = "namePublic";
+
+    function getName() external view returns(string memory) {
+        return namePrivate;
+    }
 }
